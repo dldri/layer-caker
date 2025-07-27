e6f31fa8d7f8635eaae6d556611a33b2ae9cb616
dldri <diovian.liu@gmail.com>
2025-07-27 14:09:31 +0800

    updated post type for rendering

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import { sanityFetch } from '@/sanity/lib/live';
 import { POSTS_QUERY } from '@/sanity/lib/queries';
-import type { Post } from '@/sanity/types';
+import type { POSTS_QUERYResult } from '@/sanity/types';
+
+type PostQueryResult = POSTS_QUERYResult[number];
 
 export default async function Page() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
@@ -10,7 +12,7 @@ export default async function Page() {
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <h1 className="font-bold text-4xl">Post index</h1>
       <ul className="grid grid-cols-1 divide-y divide-blue-100">
-        {posts.map((post: Post) => (
+        {posts.map((post: PostQueryResult) => (
           <li key={post._id}>
             <Link
               className="block p-4 hover:text-blue-500"
